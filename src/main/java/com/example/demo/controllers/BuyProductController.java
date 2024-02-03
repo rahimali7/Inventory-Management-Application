4fb244b25191fa10cd3d6ff822cd997579dd0981
abdirahim7 <aabdirahim7@gmail.com>
2024-02-03 18:03:38 -0500

    mainscreen.html: line 87, I added a buy now button.

---
+++ src/main/java/com/example/demo/controllers/BuyProductController.java
@@ -0,0 +1,56 @@
+package com.example.demo.controllers;
+
+
+
+import com.example.demo.domain.Product;
+import com.example.demo.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+
+@Controller
+public class BuyProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @PostMapping("/buyProduct")
+    public String buyProduct(@RequestParam("productID") int theId, Model model) {
+        Long theidl = (long) theId;
+
+        Optional<Product> result = productRepository.findById(theidl);
+
+        Product theproduct = null;
+
+        if (result.isPresent()) {
+            theproduct = result.get();
+        }
+        assert theproduct != null;
+        if (theproduct.getInv() > 0) {
+            theproduct.setInv(theproduct.getInv() - 1);
+            productRepository.save(theproduct);
+            return displayPurchasePage();
+        }
+        else {
+            return errorPurchasePage();
+        }
+
+    }
+
+    @GetMapping("/displayPurchasePage")
+    public String displayPurchasePage() {
+        return "confirmationbuyproduct";
+    }
+
+    @GetMapping("/errorPurchasePage")
+    public String errorPurchasePage() {
+        return "producterrornavigator";
+    }
+
+}
